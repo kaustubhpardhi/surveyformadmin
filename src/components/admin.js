807ae3935c82c179789f3b9075d8624d60c316f6
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-02-14 13:57:57 +0530

    logo updated

--- src/components/admin.js
+++ src/components/admin.js
@@ -14,7 +14,7 @@ import VisibilityTwoToneIcon from "@mui/icons-material/VisibilityTwoTone";
 import DownloadExcelButton from "./DownloadExcelButton";
 import "./admin.css";
 import logo from "./meerut.jpg";
-import bob from "./bob.png";
+import bob from "./bob.webp";
 const Admin = () => {
   const [forms, setForms] = useState([]);
   useEffect(() => {
